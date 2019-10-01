dee2002332d6074888cef1e66b007c699af6cef1
github username <github email address>
2019-10-01 12:18:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
+    array = []
+    array[0] = firstName
+    array[1] = familyName
+    array[2] = title
+    array[3] = payPerHour
+    timeInEvents = []
+    timeOutEvents = []
+}
