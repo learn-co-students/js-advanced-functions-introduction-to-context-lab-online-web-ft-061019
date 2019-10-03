686a3c21983295aee10e2b277c97406ca505040f
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-10-03 14:45:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title, payPerHour){
+    var object = {
+        0: 'firstName',
+        1: 'familyName',
+        2: 'title',
+        3: 'payPerHour',
+        4: timeInEvents = [],
+        5: timeOutEvents = []
+    }
+    return Object.keys(object)
+}
